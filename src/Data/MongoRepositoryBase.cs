6a5551ed30a2dde7351311e3540260925c445a91
Steven Swenson <steven.swenson@rackspace.com>
2016-01-15 14:55:53 -0500

    Fix connection string.

--- src/Data/MongoRepositoryBase.cs
+++ src/Data/MongoRepositoryBase.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Configuration;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 
 namespace FileAuditManager.Data
 {
     abstract class MongoRepositoryBase
     {
-        private static string mongoServerName;
-        private static string mongoDatabaseName;
-
-        protected readonly IMongoClient MongoClient;
+        private readonly IMongoClient MongoClient;
         protected readonly IMongoDatabase MongoDatabase;
 
         protected MongoRepositoryBase(string connectionString, IMongoDatabase database)
@@ -20,35 +15,9 @@ namespace FileAuditManager.Data
             }
             else
             {
-                ParseConnectionString(connectionString);
-                MongoClient = new MongoClient(mongoServerName);
-                MongoDatabase = MongoClient.GetDatabase(mongoDatabaseName);
-            }
-        }
-
-        private static void ParseConnectionString(string connectionString)
-        {
-            if (string.IsNullOrWhiteSpace(mongoServerName) || string.IsNullOrWhiteSpace(mongoDatabaseName))
-            {
-                var parts = connectionString.Split(';');
-                foreach (var part in parts)
-                {
-                    var halves = part.Split('=');
-                    if (halves.Length != 2) throw new ConfigurationErrorsException("Invalid mongodb connection string.");
-                    if (halves[0].Equals("server", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        mongoServerName = halves[1];
-                    }
-                    else if (halves[0].Equals("database", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        mongoDatabaseName = halves[1];
-                    }
-                }
-
-                if (string.IsNullOrWhiteSpace(mongoServerName) || string.IsNullOrWhiteSpace(mongoDatabaseName))
-                {
-                    throw new ConfigurationErrorsException("Invalid mongodb connection string. You must specify a server and a database.");
-                }
+                var mongoUri = new MongoUrl(connectionString);
+                MongoClient = new MongoClient(mongoUri);
+                MongoDatabase = MongoClient.GetDatabase(mongoUri.DatabaseName);
             }
         }
     }
