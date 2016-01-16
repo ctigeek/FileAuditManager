d85ad007e20c80f380ede793c95b3e5558396864
Steven Swenson <steven.swenson@rackspace.com>
2016-01-16 13:35:03 -0500

    Move FileAuditManagerHttpControllerActivator.cs.

--- src/Controllers/FileAuditManagerHttpControllerActivator.cs
+++ src/Controllers/FileAuditManagerHttpControllerActivator.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
 
-namespace FileAuditManager
+namespace FileAuditManager.Controllers
 {
     class FileAuditManagerHttpControllerActivator : IHttpControllerActivator
     {
